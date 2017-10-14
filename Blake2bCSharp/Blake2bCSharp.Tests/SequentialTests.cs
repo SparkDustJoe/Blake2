491411252f5893cb1a0d48464b436c360aebc2ea
SparkDustJoe <sparkdustjoe+github@gmail.com>
2017-10-13 21:30:29 -0400

    Initial Commit Passes known answer tests.  Could use a bit of optimization.

---
+++ Blake2bCSharp/Blake2bCSharp.Tests/SequentialTests.cs
@@ -0,0 +1,89 @@
+﻿// Original Written in 2012 by Christian Winnerlein  <codesinchaos@gmail.com>
+// Rewritten Fall 2015
+//   by Dustin Sparks <sparkdustjoe@gmail.com>
+
+
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+/*
+  Based on BlakeSharp
+  by Dominik Reichl <dominik.reichl@t-online.de>
+  Web: http://www.dominik-reichl.de/
+  If you're using this class, it would be nice if you'd mention
+  me somewhere in the documentation of your program, but it's
+  not required.
+
+  BLAKE was designed by Jean-Philippe Aumasson, Luca Henzen,
+  Willi Meier and Raphael C.-W. Phan.
+  BlakeSharp was derived from the reference C implementation.
+*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Blake2bCSharp.Tests
+{
+    [TestClass]
+    public class SequentialTests
+    {
+        byte[] input = Enumerable.Range(0, 256).Select(i => (byte)i).ToArray();
+
+
+        [TestMethod]
+        public void CheckTestVectors()
+        {
+            for (int len = 0; len < TestVectors.UnkeyedBlake2B.Length; len++)
+            {
+                var input = Enumerable.Range(0, len).Select(i => (byte)i).ToArray();
+                var hash = Blake2bCSharp.Blake2B.ComputeHash(input);
+                string actual = BitConverter.ToString(hash).Replace("-", "");
+                string expected = TestVectors.UnkeyedBlake2B[len];
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        public void CheckKeyedTestVectors()
+        {
+            var key = Enumerable.Range(0, 64).Select(i => (byte)i).ToArray();
+            for (int len = 0; len < TestVectors.KeyedBlake2B.Length; len++)
+            {
+                var input = Enumerable.Range(0, len).Select(i => (byte)i).ToArray();
+                var hash = Blake2bCSharp.Blake2B.ComputeHash(input, new Blake2bCSharp.Blake2bConfig { Key = key });
+                string actual = BitConverter.ToString(hash).Replace("-", "");
+                string expected = TestVectors.KeyedBlake2B[len];
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        public void Splits()
+        {
+            var hasher = Blake2B.Create();
+            for (int len = 0; len <= 256; len++)
+            {
+                hasher.Init();
+                hasher.Update(input, 0, len);
+                string hash0 = BitConverter.ToString(hasher.Finish());
+
+                for (int split1 = 0; split1 <= len; split1++)
+                {
+                    for (int split2 = split1; split2 <= len; split2++)
+                    {
+                        hasher.Init();
+                        hasher.Update(input, 0, split1);
+                        hasher.Update(input, split1, split2 - split1);
+                        hasher.Update(input, split2, len - split2);
+                        string hash1 = BitConverter.ToString(hasher.Finish());
+                        Assert.AreEqual(hash0, hash1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Blake2bCSharp/IvBuilder.cs
@@ -0,0 +1,91 @@
+﻿// Original Written in 2012 by Christian Winnerlein  <codesinchaos@gmail.com>
+// Rewritten Fall 2015
+//   by Dustin J Sparks <sparkdustjoe@gmail.com>
+
+
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+/*
+  Based on BlakeSharp
+  by Dominik Reichl <dominik.reichl@t-online.de>
+  Web: http://www.dominik-reichl.de/
+  If you're using this class, it would be nice if you'd mention
+  me somewhere in the documentation of your program, but it's
+  not required.
+
+  BLAKE was designed by Jean-Philippe Aumasson, Luca Henzen,
+  Willi Meier and Raphael C.-W. Phan.
+  BlakeSharp was derived from the reference C implementation.
+*/
+
+using System;
+
+namespace Blake2bCSharp
+{
+    /* from the ref C, since we are not doing any tree hashing, we care about '+' of the following:
+    typedef struct __blake2b_param
+    {
+        uint8_t  digest_length; // 1 +
+        uint8_t  key_length;    // 2 +
+        uint8_t  fanout;        // 3 =1
+        uint8_t  depth;         // 4 =1
+        uint32_t leaf_length;   // 8 =0
+        uint64_t node_offset;   // 16 =0
+        uint8_t  node_depth;    // 17 =0
+        uint8_t  inner_length;  // 18 =0
+        uint8_t  reserved[14];  // 32
+        uint8_t  salt[BLAKE2B_SALTBYTES]; // 48 +
+        uint8_t  personal[BLAKE2B_PERSONALBYTES];  // 64 +
+    } blake2b_param;
+    //*/
+
+    internal static class Blake2bIvBuilder
+    {
+        public static UInt64[] ConfigB(Blake2bConfig config)
+        {
+
+            var rawConfig = new UInt64[8]; //
+            //var result = new UInt64[8]; //
+
+            //digest length
+            if (config.OutputSizeInBytes <= 0 | config.OutputSizeInBytes > 64) //
+                throw new ArgumentOutOfRangeException("config.OutputSize");
+            rawConfig[0] = (UInt32)config.OutputSizeInBytes; //
+
+            //Key length
+            if (config.Key != null)
+            {
+                if (config.Key.Length > 64) //
+                    throw new ArgumentException("config.Key", "Key too long");
+                rawConfig[0] |= (UInt64)(config.Key.LongLength << 8); //
+            }
+            // Fan Out =1 and Max Height / Depth = 1
+            rawConfig[0] |= 1 << 16;
+            rawConfig[0] |= 1 << 24;
+            // Leaf Length and Inner Length 0, no need to worry about them
+            // Salt
+            if (config.Salt != null)
+            {
+                if (config.Salt.Length != 16)
+                    throw new ArgumentException("config.Salt has invalid length");
+                rawConfig[4] = Blake2bCore.BytesToUInt64(config.Salt, 0);
+                rawConfig[5] = Blake2bCore.BytesToUInt64(config.Salt, 8);
+            }
+            // Personalization
+            if (config.Personalization != null)
+            {
+                if (config.Personalization.Length != 16)
+                    throw new ArgumentException("config.Personalization has invalid length");
+                rawConfig[6] = Blake2bCore.BytesToUInt64(config.Personalization, 0);
+                rawConfig[7] = Blake2bCore.BytesToUInt64(config.Personalization, 8);
+            }
+
+            return rawConfig;
+        }
+
+     }
+}
